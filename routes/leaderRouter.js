771ee12db5210d4f6146b007a1414b1b5686e027
LWJgithub <15818464582@163.com>
2016-10-15 12:06:39 +0800

    Update leaderRouter.js

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,53 +1,77 @@
 /**
- * Created by Joh on 2016/10/10.
+ * leaderRouter
  */
 var express = require('express');
-var http = require('http'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser');
-
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var Leadership = require('../models/leaderships')
 var hostname = 'localhost';
-var port = 3000;
+ var port = 3000;
 
 var app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-app.all('/leadship', function (req, res, next) {
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    next();
+var leaderRouter = express.Router();
 
-});
+leaderRouter.use(bodyParser.json());
 
-app.get('/leadship',function (req, res, next) {
-    res.end('Will send all the dishes to you');
-});
+leaderRouter.route('/')
+    .get(function(req,res,next){
+        Leadership.find({}, function (err, leadership) {
+            if (err) throw(err);
 
-app.post('/leadship',function (req, res, next) {
-    res.end('will add  dishes：' + req.body.name + ' with details: '+ req.body.description);
-});
+            res.json(leadership);
+        });
+    })
 
-app.delete('/leadship', function (req, res ,next) {
-    res.end('delete all the dishes');
-});
+    .post(function(req, res, next){
+        Leadership.create(req.body, function (err, leadership) {
+            if (err)throw(err);
+            console.log("Created Leadership");
+            var id = leadership._id;
+            res.writeHead(200,{"Content-Type": "text/plain"});
 
-app.get('/leadship/:leadshipId',function (req, res, next) {
-    res.end('will send details of the dish '+ req.params.leadshipId+'to you; ');
-});
+            res.end('add the  leadershipes with id:'+ id);
+        });
+    })
+    .delete(function(req, res, next){
+        Leadership.remove({}, function (err, resp) {
+            if (err)throw(err);
+            res.json(resp);
+        });
+    });
 
-app.put('/leadship/:leadshipId', function(req, res, next) {
-    res.write('update the dish: '+req.params.leadshipId+ "\n ");
-    res.end('update the dishes：' + req.body.name + ' with details: '+req.body.description);
-});
+leaderRouter.route('/:leadershipId')
+    .get(function(req,res,next){
+        Leadership.findById(req.params.leadershipId, function (err, leadership) {
+            if (err)throw(err);
+            res.json(leadership);
+        });
+    })
 
-app.delete('/leadship/:leadshipId', function (req, res, next) {
-    res.end('deleting the dishes: '+req.params.leadshipId);
-} );
+    .put(function(req, res, next){
+        Leadership.findByIdAndUpdate(req.params.leadershipId, {
+            $set:req.body
+        },{new: true},function (err, leadership) {
+            if (err)throw(err);
+            res.json(leadership);
+        })
+    })
 
-app.use(express.static(__dirname+'/public'));
+    .delete(function(req, res, next){
+        Leadership.findByIdAndRemove(req.params.leadershipId, function (err, resp) {
+            if (err)throw(err);
+            res.json(resp);
+        });
+    });
 
-app.listen(port, hostname, function () {
-    console.log('server is listenning at http://${hostname}: ${port}/');
-});
 
+app.use('/leaderships',leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+ app.listen(port, hostname, function(){
+ console.log('Server running at http:/'+hostname+':'+port+'/');
+ });
+module.exports = leaderRouter;
